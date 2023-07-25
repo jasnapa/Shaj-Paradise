961913a92f3770f1738582376967a29022debc4c
jasnapa <jasnajass661@gmail.com>
2023-07-25 11:53:57 +0530

    yup added

--- client/src/components/Admin/AdminLogin.jsx
+++ client/src/components/Admin/AdminLogin.jsx
@@ -3,6 +3,7 @@ import { useFormik } from "formik";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import * as Yup from 'yup';
 
 
 
@@ -18,11 +19,22 @@ const AdminLogin = ()=>{
         setShowPassword(!showPassword);
     };
 
+    const validate = Yup.object({
+        email: Yup.string()
+            .email('invalid email address')
+            .required('Email is required'),
+        password: Yup.string()
+            .required("Password is required"),
+    })
+
     const formik = useFormik({
         initialValues: {
           email: '',
           password: '',
         },
+
+        validationSchema: validate,
+
         onSubmit:async (values) => {
             try {
                 console.log(email);
@@ -68,14 +80,21 @@ const AdminLogin = ()=>{
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                         </svg>
-                        <input className="pl-2 outline-none border-none" type="email" name="email" id="email" onChange={formik.handleChange} placeholder="Email Address" required/>
+                        <input className="pl-2 outline-none border-none" type="email" name="email" id="email" onChange={formik.handleChange} placeholder="Email Address" />
                     </div>
+                    {formik.touched.email && formik.errors.email ? (
+                                <div className='text-red-500' >{formik.errors.email}</div>
+                            ) : null}
                     <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                         </svg>
-                        <input className="pl-2 outline-none border-none" type='password' name="password" id="password" onChange={formik.handleChange} placeholder="Password" required/>
+                        <input className="pl-2 outline-none border-none" type='password' name="password" id="password" onChange={formik.handleChange} placeholder="Password" />
+                        
                     </div>
+                    {formik.touched.password && formik.errors.password ? (
+                                    <div className='text-red-500'>{formik.errors.password}</div>
+                                ) : null}
                     <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
                 </form>
             </div>
