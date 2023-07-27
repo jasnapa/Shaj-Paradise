0c94cfcfdeee17cbbed6094150f5428527aa5221
jasnapa <jasnajass661@gmail.com>
2023-07-27 20:10:22 +0530

    added vendor home page

---
+++ client/src/Pages/Vendor/VendorHomePage.jsx
@@ -0,0 +1,13 @@
+import VendorHome from "../../components/Vendor/VendorHome"
+import React from 'react';
+
+
+
+const VendorHomePage=()=> {
+    return(
+        <VendorHome />
+    )
+}
+
+
+export default VendorHomePage
