f587ed2a04222adbdef7a2767fc4a5e6206ace9d
jasnapa <jasnajass661@gmail.com>
2023-07-27 20:09:34 +0530

    added user resort page

---
+++ client/src/Pages/User/UserResortPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import UserResort from "../../components/User/UserResort"
+
+
+
+const UserResortPage=()=> {
+    return(
+        <UserResort />
+    )
+}
+export default UserResortPage
