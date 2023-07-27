abd867e3b614726f21eaecc700156163a9f30973
jasnapa <jasnajass661@gmail.com>
2023-07-27 20:05:49 +0530

    userhome added

---
+++ client/src/components/User/UserHome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./Banner/Banner";
+import { Navbar } from "./Navbar/Navbar";
+
+const UserHome = () => {
+  return (
+    <>
+    <Navbar/>
+     <Banner />
+    </>
+  );
+};
+
+export default UserHome;
