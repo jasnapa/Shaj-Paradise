8cc3fa00dbec4a271a8236b5b57c72d5859ee88c
jasnapa <jasnajass661@gmail.com>
2023-08-02 14:07:56 +0530

    added package component

---
+++ client/src/components/User/UserPackage.jsx
@@ -0,0 +1,57 @@
+import { Link, useParams } from "react-router-dom";
+import { Navbar } from "./Navbar/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function UserPackage() {
+  const [packages, setPackages] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    try {
+      (async function () {
+        const { data } = await axios.get("/packages/" + id);
+        console.log(data.packages);
+        if (data.success) {
+          setPackages(data.packages);
+        }
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {
+       packages.map((item) => {
+        return (
+        <Link to ={"/viewResorts"} state={{ data: item }}>
+          <div className="inline-block ml-10 mt-10">
+            <div className="card w-52 glass">
+            {item.resort.images && item.resort.images[0] ? (
+              <figure className="w-fit h-40">
+                <img src={item.resort.images[0]} alt="car!" />
+              </figure>
+              ) : (
+                <p>Loading images...</p>
+              )}
+              <div className="card-body">
+                <h2 className="card-title">{item.amount}</h2>
+                <p>{item.daysInfo}</p>
+                <div className="card-actions justify-end">
+                  {/* <button className="btn btn-primary">Learn now!</button> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+        );
+      })}
+    </>
+  );
+}
+
+export default UserPackage;
