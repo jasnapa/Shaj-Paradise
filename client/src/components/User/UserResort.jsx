a92227ff7becc15ed8e0c3826f1c071c646de14a
jasnapa <jasnajass661@gmail.com>
2023-08-02 14:08:24 +0530

    added search query

--- client/src/components/User/UserResort.jsx
+++ client/src/components/User/UserResort.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import { Navbar } from "./Navbar/Navbar";
 import resort from "./assets/resort1.jpg";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 
 function UserResort() {
 
 
   const [vendors, setResorts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
 		try {
 		  (async function () {
@@ -22,27 +25,48 @@ function UserResort() {
 		}
 	  },[])
 		
+    const handleSearch = async () => {
+      try {
+        const { data } = await axios.get(`/search`);
+        if (data.success) {
+          setResorts(data.vendors);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
   return (
     <>
       <Navbar />
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search for resorts..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       { 
       vendors.map((item) =>{
         return(
-      <div className="inline-block ml-10 mt-10">
+      <Link to ={"/userPackages/" + item._id }>
+        <div className="inline-block ml-10 mt-10">
         <div className=" card w-52 glass">
           <figure className="w-fit h-40">
-            <img src={resort} alt="car!" />
+            <img src={item.images[0]} alt="car!" />
           </figure>
           <div className="card-body">
             <h2 className="card-title">{item.resortName}</h2>
-            <p>{item.description}</p>
+            <p>{item.place}</p>
             <div className="card-actions justify-end">
               {/* <button className="btn btn-primary">Learn now!</button> */}
             </div>
           </div>
         </div>
-      </div>
+      </div> 
+      </Link>
          )
         })
 }
