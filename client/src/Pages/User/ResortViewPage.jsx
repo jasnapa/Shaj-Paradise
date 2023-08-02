602b515ba66e76e8f906c530a78cd70ebc95531e
jasnapa <jasnajass661@gmail.com>
2023-08-02 14:14:19 +0530

    addedd resort view page

---
+++ client/src/Pages/User/ResortViewPage.jsx
@@ -0,0 +1,12 @@
+import ResortView from "../../components/User/ResortView/ResortView"
+
+
+
+
+
+const ResortViewPage=()=> {
+    return(
+        <ResortView />
+    )
+}
+export default ResortViewPage
