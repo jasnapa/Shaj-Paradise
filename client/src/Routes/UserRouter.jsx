9629ccd116feeb71563f5c39f8db8fa4965db4f1
jasnapa <jasnajass661@gmail.com>
2023-08-05 13:55:35 +0530

    removed unwanted things

--- client/src/Routes/UserRouter.jsx
+++ client/src/Routes/UserRouter.jsx
@@ -5,22 +5,29 @@ import UserSignupPage from "../Pages/User/UserSighupPage";
 import EmailVerifyPage from "../Pages/User/EmailVerifyPage";
 import UserResortPage from "../Pages/User/UserResortPage";
 import HomePage from "../Pages/User/HomePage";
+import ResortViewPage from "../Pages/User/ResortViewPage";
+import UserPackagePage from "../Pages/User/UserPackagePage";
+
 
 
 
 
 
 function UserRouter(){
+    
     return(
         <Routes>
+
+            {/* <Route element = {<PrivateRoutes role={'user'} route={'/login'}/>}> 
+            </Route> */}
 
             <Route path="/login" element={<UserLoginPage/>}/>
             <Route path="/signup" element={<UserSignupPage/>}/>
             <Route path="/verifyMail" element={<EmailVerifyPage />} />
             <Route path="/" element={<HomePage />}/>
             <Route path="/resorts" element={<UserResortPage />}/>
-         
-
+            <Route path="/viewResorts" element={<ResortViewPage />}/>
+            <Route path="/userPackages/:id" element={<UserPackagePage />}/>
         </Routes>
     )
 }
