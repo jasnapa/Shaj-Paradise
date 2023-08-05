60832cbe1d3e44423fbf7d2a6009da8af0b772ea
jasnapa <jasnajass661@gmail.com>
2023-08-05 13:53:16 +0530

    added signup page

---
+++ client/src/Pages/Vendor/VendorSignupPage.jsx
@@ -0,0 +1,15 @@
+import VendorSignup from "../../components/Vendor/VendorSignup"
+
+
+
+
+
+
+const VendorSignupPage= ()=>{
+    
+    return(
+        <VendorSignup />
+    )
+}
+
+export default VendorSignupPage
