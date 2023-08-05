6d5d080538dbf8d48b0c69135409d50030e3781b
jasnapa <jasnajass661@gmail.com>
2023-08-05 13:55:53 +0530

    added route

--- client/src/Routes/VendorRouter.jsx
+++ client/src/Routes/VendorRouter.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import VendorLoginPage from "../Pages/Vendor/VendorLoginPage";
 import VendorHomePage from "../Pages/Vendor/VendorHomePage";
+import PrivateRoutes from "../utils/PrivateRoutes";
+import VendorWelcomePage from "../Pages/Vendor/VendorWelcomePage";
+import VendorPackagePage from "../Pages/Vendor/VendorResortsPage";
+import VendorSignupPage from "../Pages/Vendor/VendorSignupPage";
+import VendorResortsPage from "../Pages/Vendor/VendorResortsPage";
 
 
 
@@ -9,9 +14,14 @@ import VendorHomePage from "../Pages/Vendor/VendorHomePage";
 function VendorRouter(){
     return(
         <Routes>
-            <Route path="login" element={<VendorLoginPage/>}/>
+            <Route element = {<PrivateRoutes role={'vendor'} route={'/vendor/login'}/>} >
             <Route path="home" element={<VendorHomePage/>}/>
-            
+            <Route path="welcome" element={<VendorWelcomePage/>}/>
+            <Route path="resorts" element={<VendorResortsPage/>}/>
+            </Route>
+          
+            <Route path="login" element={<VendorLoginPage/>}/>
+            <Route path="signup" element={<VendorSignupPage/>}/>
         </Routes>
     )
 }
