07b87f7c8f654193bf557f547bfef73d43f35819
jasnapa <jasnajass661@gmail.com>
2023-08-05 13:52:47 +0530

    added page

---
+++ client/src/Pages/Vendor/VendorResortsPage.jsx
@@ -0,0 +1,18 @@
+import VendorResorts from "../../components/Vendor/VendorResorts"
+
+
+
+
+
+
+
+
+
+const VendorResortsPage= ()=>{
+    
+    return(
+        <VendorResorts/>
+    )
+}
+
+export default VendorResortsPage
