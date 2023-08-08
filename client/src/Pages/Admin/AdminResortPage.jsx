fa001b0d3121eafd04e66f3e7f5982d11cfb2273
jasnapa <jasnajass661@gmail.com>
2023-08-08 23:57:57 +0530

    added admin resort page

---
+++ client/src/Pages/Admin/AdminResortPage.jsx
@@ -0,0 +1,14 @@
+import AdminResort from "../../components/Admin/AdminResort"
+
+
+
+
+
+const AdminResortPage = ()=>{
+
+    return(
+        <AdminResort />
+    )
+}
+
+export default AdminResortPage 
