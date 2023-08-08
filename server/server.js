0e0cadcc84023989b4c309d39aaea7e1b09d348a
jasnapa <jasnajass661@gmail.com>
2023-08-09 00:08:12 +0530

    bug fixed

--- server/server.js
+++ server/server.js
@@ -29,9 +29,9 @@ app.use(
   
 
 
-app.use(express.json({limit:"10mb"}))
+app.use(express.json({limit:"50mb"}))
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true, limit:"50mb"}))
 
 
 
