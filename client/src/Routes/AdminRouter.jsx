e08af138cf234da48295e6347e1e82bf8a357134
jasnapa <jasnajass661@gmail.com>
2023-08-09 00:02:47 +0530

    added route

--- client/src/Routes/AdminRouter.jsx
+++ client/src/Routes/AdminRouter.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AdminLoginPage from "../Pages/Admin/AdminLoginPage";
 import AdminDashPage from "../Pages/Admin/AdminDashPage";
-import AdminUsersPage from "../Pages/Admin/AdminUserPage";
 import AdminVendorPage from "../Pages/Admin/AdminVendorPage";
+import PrivateRoutes from "../utils/PrivateRoutes";
+import AdminUsersPage from "../Pages/Admin/AdminUserPage";
+import AdminResortPage from "../Pages/Admin/AdminResortPage";
 
 
 
@@ -12,11 +14,16 @@ import AdminVendorPage from "../Pages/Admin/AdminVendorPage";
 function AdminRouter(){
     return(
         <Routes>
-            <Route path="login" element={<AdminLoginPage/>}/>
-            <Route path="dashboard" element={<AdminDashPage/>}/>
-            <Route path="users" element={<AdminUsersPage/>}/>
+
+            <Route element = {<PrivateRoutes role={'admin'} route={'/admin/login'}/>} >
+
+            <Route path="dashboard" element={<AdminDashPage />}/>
+            <Route path="users" element={<AdminUsersPage />}/>
             <Route path="vendors" element={<AdminVendorPage/>}/>
-            
+            <Route path="resort" element={<AdminResortPage/>}/>
+            </Route>
+            <Route path="login" element={<AdminLoginPage/>}/>
+
         </Routes>
     )
 }
