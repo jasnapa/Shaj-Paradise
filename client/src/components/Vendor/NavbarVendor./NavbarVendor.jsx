e8386ab74316613e4dee41d12fef86bd20d8f020
jasnapa <jasnajass661@gmail.com>
2023-08-08 23:57:24 +0530

    make changes in component

--- client/src/components/Vendor/NavbarVendor./NavbarVendor.jsx
+++ client/src/components/Vendor/NavbarVendor./NavbarVendor.jsx
@@ -2,27 +2,39 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { authVendor, getVendor } from "../../../Services/vendorApi";
+import { setVendorDetails, setVendorSignout } from "../../../redux/Features/vendorSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
 
 function NavbarVendor() {
-  const [vendors, setResorts] = useState([]);
-  useEffect(() => {
-		try {
-		  (async function () {
-			const { data } = await getVendor()
-			console.log(data);
-			if (data.success) {
-			  setResorts(data.vendors);
-			}
-		  })();
-		} catch (error) {
-		  console.log(error);
-		}
-	  },[])
+
+const navigate = useNavigate()
+const vendor = useSelector((state) => state.vendor)
+const dispatch= useDispatch()
+console.log("kkjj",vendor);
+
+useEffect( ()=>{
+  if(!vendor.id){
+    authVendor().then((response) => {
+      console.log(response.data);
+      if (response.data.status) {
+        dispatch(
+          setVendorDetails({
+            name: response.data.vendor.vendorName,
+            id: response.data.vendor._id,
+            email: response.data.vendor.email, 
+            mobile : response.data.vendor.mobile,
+          })
+        );
+      }
+    })
+  }
+})
 
   return (
+    <>
     <div className=" navbar shadow-xl bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
@@ -72,10 +84,40 @@ function NavbarVendor() {
           </li>
         </ul>
       </div>
+      {
+          vendor.id && vendor.id ?
+          <div className="navbar-end">
+            <div className="dropdown ml-72 dropdown-end mr-4">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://source.unsplash.com/100x100/?portrait" />
+                </div>
+              </label>
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <Link to={'/profile'}>
+                  <p className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </p></Link>
+                </li>
+                <li><Link to={'/history'}><a>History</a></Link></li>
+                <li onClick={
+                  () => {
+                    localStorage.removeItem('VendorJwtKey');
+                    navigate('/vendor/login')
+                    dispatch(setVendorSignout())  
+                  }}><a>Logout</a></li>
+              </ul>
+            </div>
+          </div>
+            :
       <div className="navbar-end">
-        <Link to={'/login'}><a className="btn">Log in</a></Link> 
+        <Link to={'/vendor/login'}><a className="btn">Log in</a></Link> 
       </div>
+}
     </div>
+    </>
   );
 }
 
