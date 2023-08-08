ee58a45ca39b7c9cb1cd422cdf2be8c8363c2977
jasnapa <jasnajass661@gmail.com>
2023-08-09 00:12:18 +0530

    added router

--- server/routes/userAuthRouter.js
+++ server/routes/userAuthRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { generateOTP, login, signUp } from '../controller/userAuthController.js'
+import { generateOTP, login, signUp, userAuth } from '../controller/userAuthController.js'
 
 
 const router = express.Router()
@@ -8,6 +8,7 @@ const router = express.Router()
 router.post('/verify',generateOTP)
 router.post('/signup',signUp)
 router.post('/login',login)
+router.get('/auth',userAuth)
 
 
 export default router
