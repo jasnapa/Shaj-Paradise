511b9c743e9dab0cda84eee05e83b66cbe351969
jasnapa <jasnajass661@gmail.com>
2023-08-09 00:05:55 +0530

    bug fixed

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios' ;
 const axiosInstance = (tokenName)=> {
   const instance = axios.create({
     baseURL:'http://localhost:3000/' ,
-    timeout : 5000 ,
+    timeout : 20000 ,
     headers :{
       'Content-Type' : 'application/json'
     }
@@ -16,9 +16,9 @@ const axiosInstance = (tokenName)=> {
   })
 
   // instance response interceptor
-  instance.interceptors.response.use( response => response ,
-    error => Promise.reject(error.response.data)
-    )
+  // instance.interceptors.response.use( response => response ,
+  //   error => Promise.reject(error.response.data)
+  //   )
 
     return instance
 }
