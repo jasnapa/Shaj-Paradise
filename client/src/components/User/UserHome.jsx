95b7d4273124b5880d12831df690af7e6d4d65c2
jasnapa <jasnajass661@gmail.com>
2023-08-15 15:14:06 +0530

    added palcecart

--- client/src/components/User/UserHome.jsx
+++ client/src/components/User/UserHome.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Banner from "./Banner/Banner";
 import BannerTwo from "./BannerTwo/BannerTwo"
 import Navbar from "./Navbar/Navbar";
+import UserResort from "./UserResort";
+import Testimonials from "./Testimonial/Testimonial";
+import Footer from "./Footer/Footer";
+import PlaceCart from "./PlaceCart/PlaceCart";
 
 const UserHome = () => {
   return (
     <div>
     <Navbar />
      <Banner />
+     <PlaceCart />
+     <Testimonials />
      <BannerTwo />
+     <Footer />
     </div>
   );
 };
