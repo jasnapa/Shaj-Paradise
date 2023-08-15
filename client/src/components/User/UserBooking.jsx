d9da1ed100793b45727b3a344ca09c20c0e0c6b9
jasnapa <jasnajass661@gmail.com>
2023-08-15 15:13:08 +0530

    added booking

---
+++ client/src/components/User/UserBooking.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import Navbar from "./Navbar/Navbar";
+import { useEffect } from "react";
+import axios from "axios";
+import { bookingHistory } from "../../Services/userApi";
+
+
+
+const UserBooking = () => {
+const[booking,setBooking] = useState([])
+
+  useEffect(() => { 
+    try {
+      (async function () {
+        const { data } = await bookingHistory()
+        console.log(data.booking,"jjjj")
+        if (data.success) {
+          setBooking(data.booking);
+        }
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  
+
+  
+
+
+  return (
+    <>
+      <Navbar />
+
+      <section class="py-1 bg-blueGray-50">
+        <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+          <div class="relative flex px-10 py-10 flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+            <div class="rounded-t mb-0 px-4 py-3 border-0">
+              <div class="flex flex-wrap items-center">
+                <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 class="font-semibold text-base text-blueGray-700">
+                    Booking
+                  </h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="block w-full overflow-x-auto ml-10">
+              <table class="items-center bg-transparent w-full border-collapse ">
+                <thead>
+                  <tr>
+                    <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Id
+                    </th>
+                    <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Date
+                    </th>
+                    <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Resort
+                    </th>
+                    <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Amount
+                    </th>
+                    <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      CheckIn
+                    </th>
+                        <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle  py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      CheckOut
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                  booking.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                          {index + 1}
+                        </th>
+                        <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                          {item.date}
+                        </th>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                          {item.resort.resortName}
+                        </td>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                          {item.resort.amount}
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          {item.checkin}
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          {item.checkout}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default UserBooking;
