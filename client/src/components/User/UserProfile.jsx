ef9e3421badb97863bf36ed768ba7f68114c2740
jasnapa <jasnajass661@gmail.com>
2023-08-24 21:21:08 +0530

    added user profile

--- client/src/components/User/UserProfile.jsx
+++ client/src/components/User/UserProfile.jsx
@@ -1,74 +1,191 @@
 import { useEffect, useState } from "react";
 import Navbar from "./Navbar/Navbar";
-import { userProfile } from "../../Services/userApi";
-
-
+import { saveProfile, userProfile } from "../../Services/userApi";
+import { useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 function UserProfile() {
-    const[profile,setProfile]=useState([])
+  const [refresh, setRefresh] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const profile = useSelector((state) => state.user);
 
- useEffect(() => { 
-    try {
-      (async function () {
-        const { data } = await userProfile();
-        console.log(data,"jjjj")
+  const validate = Yup.object({
+    name: Yup.string()
+      .max(15, "Must be 15 characters or less")
+      .required("name Required"),
+    mobile: Yup.string()
+      .max(10, "Mobile number not valid")
+      .min(10, "Mobile number not valid")
+      .required("mobile Required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: profile.name,
+      mobile: profile.mobile,
+    },
+    validationSchema: validate,
+
+    onSubmit: async (values) => {
+      try {
+        console.log(values, "ghjghgjh");
+        const { data } = await saveProfile(values);
         if (data.success) {
-          setProfile(data.profile);
+          setRefresh(!refresh);
+          setEdit(false);
+          toast.success(data.message, {
+            position: "top-center",
+          });
+        } else {
+          toast.error(data.message, {
+            position: "top-center",
+          });
         }
-      })();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-  if (!profile) {
-    return <p>Loading...</p>;
-  }
-
-
-    return (
-        <>
-        <Navbar />
-        { profile.map((item,key) =>(
-            <div key={item._id} className="flex items-center h-screen w-full justify-center">
-        <div className="flex items-center h-screen w-full justify-center">
-            <div className="max-w-xs">
-                <div className="bg-white shadow-xl rounded-lg py-3">
-                    <div className="photo-wrapper p-2">
-                        <img className="w-32 h-32 rounded-full mx-auto" src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="John Doe" />
-                    </div>
-                    <div className="p-2">
-                        <h3 className="text-center text-xl text-gray-900 font-medium leading-8">{item.name}</h3>
-                        {/* <div className="text-center text-gray-400 text-xs font-semibold">
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+  return (
+    <>
+      <Navbar edit={edit} refresh={refresh} />
+      {!edit ? (
+        <div className="flex items-center h-screen w-full py-11 justify-center">
+          <div className="max-w-xs">
+            <div className="bg-white card shadow-xl rounded-lg p-20">
+              <div className="photo-wrapper p-2">
+                <img
+                  className="w-32 h-32 rounded-full mx-auto"
+                  src="https://source.unsplash.com/100x100/?portrait"
+                  alt="John Doe"
+                />
+              </div>
+              <div className="p-2">
+                <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+                  {profile.name}
+                </h3>
+                {/* <div className="text-center text-gray-400 text-xs font-semibold">
                             <p>Web Developer</p>
                         </div> */}
-                        <table className="text-xs my-3">
-                            <tbody>
-                                {/* <tr>
+                <table className="text-xs my-3">
+                  <tbody>
+                    {/* <tr>
                                     <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
                                     <td className="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
                                 </tr> */}
-                                <tr>
-                                    <td className="px-2 py-2 text-gray-500 font-semibold">Phone</td>
-                                    <td className="px-2 py-2">{item.mobile}</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
-                                    <td className="px-2 py-2">{item.email}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div className="text-center my-3">
-                            <a className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
-                        </div>
-                    </div>
+                    <tr>
+                      <td className="px-2 py-2 text-gray-500 font-semibold">
+                        Phone
+                      </td>
+                      <td className="px-2 py-2">{profile.mobile}</td>
+                    </tr>
+                    <tr>
+                      <td className="px-2 py-2 text-gray-500 font-semibold">
+                        Email
+                      </td>
+                      <td className="px-2 py-2">{profile.email}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="text-center my-3">
+                  <a
+                    className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
+                    onClick={() => setEdit(true)}
+                  >
+                    Edit Profile
+                  </a>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-        </div>
+      ) : (
+        formik.values && (
+          <div className="flex items-center h-screen w-full py-11 justify-center">
+            <div className="max-w-xs">
+              <div className="bg-white card shadow-xl rounded-lg p-20">
+                <div className="photo-wrapper p-2">
+                  <img
+                    className="w-32 h-32 rounded-full mx-auto"
+                    src="https://source.unsplash.com/100x100/?portrait"
+                    alt="John Doe"
+                  />
+                </div>
+                <div className="p-2">
+                  <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+                    {profile.name}
+                  </h3>
+                  {/* <div className="text-center text-gray-400 text-xs font-semibold">
+                            <p>Web Developer</p>
+                        </div> */}
+                  <form onSubmit={formik.handleSubmit}>
+                    <table className="text-xs my-3">
+                      <tbody>
+                        {/* <tr>
+                                    <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                                    <td className="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
+                                </tr> */}
+                        <tr>
+                          <td className="px-2 py-2 text-gray-500 font-semibold">
+                            Name
+                          </td>
+                          <td className="px-2 py-2">
+                            <input
+                              className="input-xs"
+                              onChange={formik.handleChange}
+                              type="text"
+                              name="name"
+                              value={formik.values.name}
+                            />
+                            {formik.touched.name && formik.errors.name ? (
+                              <div className="text-red-500">
+                                {" "}
+                                {formik.errors.name}{" "}
+                              </div>
+                            ) : null}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td className="px-2 py-2 text-gray-500 font-semibold">
+                            Mobile
+                          </td>
+                          <td className="px-2 py-2">
+                            <input
+                              className="input-xs"
+                              onChange={formik.handleChange}
+                              type="number"
+                              name="mobile"
+                              value={formik.values.mobile}
+                            />
+                            {formik.touched.mobile && formik.errors.mobile ? (
+                              <div className="text-red-500">
+                                {" "}
+                                {formik.errors.mobile}{" "}
+                              </div>
+                            ) : null}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="text-center my-3">
+                      <button
+                        className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
+                        type="submit"
+                      >
+                        Save Profile
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         )
-        )}
-        </>
-    );
-};
+      )}
+    </>
+  );
+}
 
 export default UserProfile;
