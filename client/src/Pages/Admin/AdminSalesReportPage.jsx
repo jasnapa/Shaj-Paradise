64447cef74ad8e20cee4d4ca41054c1141728b86
jasnapa <jasnajass661@gmail.com>
2023-08-31 20:51:06 +0530

    create sales report page

---
+++ client/src/Pages/Admin/AdminSalesReportPage.jsx
@@ -0,0 +1,15 @@
+import AdminSalesReport from "../../components/Admin/AdminSalesReport"
+
+
+
+
+
+
+const AdminSalesReportPage= ()=>{
+
+    return(
+        <AdminSalesReport />
+    )
+}
+
+export default AdminSalesReportPage 
