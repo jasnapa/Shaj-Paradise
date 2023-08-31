c60a891adbef32474c5d3d7fc60ad2270dc2084a
jasnapa <jasnajass661@gmail.com>
2023-08-31 20:52:14 +0530

    added admin api

--- client/src/Services/adminApi.js
+++ client/src/Services/adminApi.js
@@ -3,3 +3,8 @@ import axiosInstance from "../axios/axios"
 export const authAdmin = ()=>{
     return axiosInstance("AdminJwtKey").get('/admin/auth')
 }
+
+
+export const getStats = ()=>{
+    return axiosInstance("AdminJwtKey").get('/admin/getStats')
+}
