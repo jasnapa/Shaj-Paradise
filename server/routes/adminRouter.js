1c70c4d41c9e6f6deea20d4617fe148ccb62d143
jasnapa <jasnajass661@gmail.com>
2023-08-31 20:54:21 +0530

    bug fixed

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { adminAuth, adminHistory, adminLogin, approveVendor, blockUser, blockVendor, unblockUser, unblockVendor, verifyResort, viewResort, viewUsers, viewVendorDetails, viewVendors } from "../controller/adminController.js";
+import { adminAuth, adminHistory, adminLogin, approveVendor, blockUser, blockVendor, getStats, unblockUser, unblockVendor, verifyResort, viewResort, viewUsers, viewVendorDetails, viewVendors } from "../controller/adminController.js";
 import { verifyUser } from "../middleware/verifyUser.js";
+import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 
 
@@ -10,13 +11,15 @@ import { verifyUser } from "../middleware/verifyUser.js";
 const router = express.Router()
 
 router.post('/login',adminLogin)
-
 router.get('/auth',adminAuth)
-router.get('/users',viewUsers)
-router.get('/vendors',viewVendors)
-router.get('/resort',viewResort)
-router.post('/viewVendors',viewVendorDetails)
-router.get('/adminHistory',adminHistory)
+
+router.get('/users',verifyAdmin,viewUsers)
+router.get('/vendors',verifyAdmin,viewVendors)
+router.get('/resort',verifyAdmin,viewResort)
+router.post('/viewVendors',verifyAdmin,viewVendorDetails)
+router.get('/adminHistory',verifyAdmin,adminHistory)
+router.get('/getStats',verifyAdmin,getStats)
+
 
 router.patch('/blockUser',blockUser)
 router.patch('/unblockUser',unblockUser)
