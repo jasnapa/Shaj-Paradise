944bb9280ff2897b13a55e21498d9d01ec822676
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:32:27 +0530

    bug fixed

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -1,20 +1,53 @@
 import ChatModel from "../models/chatModel.js";
+import UserModel from "../models/userModel.js";
+import vendorModel from "../models/vendorModel.js";
 
 
 
 
-export async function createChat(req,res) {
-    try {
-        const newChat = new ChatModel({
-            members: [req.body.senderId, req.body.receiverId],
-          });
-            const result = await newChat.save();
-            res.status(200).json(result);          
-        } catch (error) {
-        res.status(500).json(error);
-        console.log(error);
-    }   
-};
+export async function createChat(req, res) {
+  try {
+    const existingChat = await ChatModel.findOne({
+      members: {
+        $all: [req.body.vendorId, req.body.userId],
+      },
+    });
+
+    if (existingChat) {
+      res.status(200).json({ status: true, result: existingChat });
+    } else {
+      const newChat = new ChatModel({
+        members: [req.body.vendorId, req.body.userId],
+      });
+      const savedChat = await newChat.save();
+      res.status(200).json({ status: true, result: savedChat });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+}
+
+
+export async function getVendor(req, res) {
+  try {
+    let {Id} = req.params
+    const vendor = await vendorModel.findById(Id);
+    res.json({ success: true, vendor });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getUser(req, res) {
+  try {
+    let {Id} = req.params
+    const user = await UserModel.findById(Id);
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
 export async function chats(req,res) {
