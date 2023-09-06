31e8f22582c4c835e1b24a0d50e411b241a5df74
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:27:55 +0530

    added cash success page

---
+++ client/src/Pages/User/CashSuccessPage.jsx
@@ -0,0 +1,13 @@
+import CashSuccess from "../../components/User/Success/SuccessCash"
+
+
+
+
+
+
+const CashSuccessPage=()=> {
+    return(
+        <CashSuccess />
+    )
+}
+export default CashSuccessPage
