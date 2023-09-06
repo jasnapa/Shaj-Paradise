5fc464e03a8ccc9dd281d4296e1c7cfa7d48d55d
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:28:55 +0530

    added admin api

--- client/src/Services/adminApi.js
+++ client/src/Services/adminApi.js
@@ -8,3 +8,34 @@ export const authAdmin = ()=>{
 export const getStats = ()=>{
     return axiosInstance("AdminJwtKey").get('/admin/getStats')
 }
+
+
+export const adminUsers = (page, limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/users?page=${page}&limit=${limit}`)
+}
+
+
+export const adminVendors = (page, limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/vendors?page=${page}&limit=${limit}`);
+}
+
+
+export const adminResort = (page, limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/resort?page=${page}&limit=${limit}`);
+}
+
+
+export const adminPayment = (page, limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/adminHistory?page=${page}&limit=${limit}`);
+}
+
+
+
+export const adminHistory = (page,limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/adminHistory?page=${page}&limit=${limit}`);
+}
+
+
+export const adminSales = (fromDate,toDate,page, limit)=>{
+    return axiosInstance("AdminJwtKey").get(`/admin/adminHistory?fromDate=${fromDate}&toDate=${toDate}&page=${page}&limit=${limit}`);
+}
