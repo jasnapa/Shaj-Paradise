632e0fbec89594f2021bcc63fcbaa791534d095d
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:34:39 +0530

    bug fixed

--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createChat } from "../controller/chatController.js";
+import { createChat, getUser, getVendor } from "../controller/chatController.js";
 import { chats } from "../controller/chatController.js";
 import { findChats } from "../controller/chatController.js";
 
@@ -12,6 +12,8 @@ const router = express.Router()
 router.post('/createChat',createChat)
 
 router.get('/:Id', chats)
+router.get('/vendor/:Id', getVendor)
+router.get('/user/:Id', getUser)
 
 router.get('/find/:firstId/:secondId', findChats);
 
