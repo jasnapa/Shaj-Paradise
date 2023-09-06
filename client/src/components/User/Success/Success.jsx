de40da9d530bb60924b99689756fd3c97ab5b9e1
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:24:16 +0530

    bug fixed

--- client/src/components/User/Success/Success.jsx
+++ client/src/components/User/Success/Success.jsx
@@ -20,7 +20,7 @@ const PaymentSuccess = () => {
           <p className="text-gray-600 my-2">Thank you for completing your secure online payment.</p>
           <p> Have a great day! </p>
           <div className="py-10 text-center">
-           <Link to={'/booking'} ><a  className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
+           <Link to={'/booking'} ><a  className="px-12 bg-green-700 hover:bg-green-700 text-white font-semibold py-3">
               GO BACK
             </a>
            </Link>
