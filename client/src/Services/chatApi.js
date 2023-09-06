099048859a94cf85cc346d0b9173715cd505db16
jasnapa <jasnajass661@gmail.com>
2023-09-06 11:29:15 +0530

    bug fixed

--- client/src/Services/chatApi.js
+++ client/src/Services/chatApi.js
@@ -1,22 +1,30 @@
 import axiosInstance from "../axios/axios";
 
-export const createChat = (data) => axios.post('/chat/', data);
+export const createChat = (vendorId,userId) =>{
+    return axiosInstance("UserJwtKey").post('/chat/createChat', {vendorId,userId})
+}
 
 export const chatRoom = (id) =>{
     return axiosInstance("UserJwtKey").get(`/chat/${id}`);
 }
 
+export const getVendor = (id) =>{
+    return axiosInstance("UserJwtKey").get(`/chat/vendor/${id}`);
+}
+export const getUser = (id) =>{
+    return axiosInstance("VendorJwtKey").get(`/chat/user/${id}`);
+}
 
 
 export const findChat = (firstId, secondId) => axios.get(`/chat/find/${firstId}/${secondId}`);
 
-//message api
+//messages api
 
-
+ 
 export const getMessages = (id) => {
     return axiosInstance("UserJwtKey").get(`/message/${id}`);
 }
 
 export const sendMessage = (data) => {
-    return axiosInstance("UserJwtKey").post('/message/', {data})
+    return axiosInstance("UserJwtKey").post('/message/', {...data})
 }
