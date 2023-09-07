1dbb0eecfb0b3121e664a866a3ac3d049d5a722c
jasnapa <jasnajass661@gmail.com>
2023-09-07 21:08:33 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -33,13 +33,6 @@ app.use(
 
   dbConnect()
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://shajparadise.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-
   /////////////// socket io ////////////////////
 
 const server = createServer(app)
