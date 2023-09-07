7692e1c6404d7bfbfdd22dec8c75340d4cfd6056
jasnapa <jasnajass661@gmail.com>
2023-09-07 21:16:00 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -81,10 +81,9 @@ io.on("connection",(socket)=>{
 ////////////////////////////////////////////////
 
 
-app.use(express.json({limit:"50mb"}))
-app.use(express.json({limit:"50mb"}))
+app.use(express.json({limit:"150mb"}))
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true, limit:"50mb"}))
+app.use(express.urlencoded({extended:true, limit:"150mb"}))
 
 
 
